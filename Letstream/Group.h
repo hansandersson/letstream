df445c1f279a9713002ccfc354790fcd32e0131d
Hans Andersson <hans@Thor.local>
2011-06-14 15:14:54 -0500

    Fix Group .h .m

---
+++ Letstream/Group.h
@@ -0,0 +1,20 @@
+//
+//  Group.h
+//  Letstream
+//
+//  Created by Hans Andersson on 11/06/14.
+//  Copyright (c) 2011 Ultramentem & Vigorware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import "Record.h"
+
+@class Search;
+
+@interface Group : Record {}
+
+@property (nonatomic, retain) NSNumber *identifier;
+@property (nonatomic, retain) NSSet *searches;
+
+@end
---
+++ Letstream/Group.m
@@ -0,0 +1,16 @@
+//
+//  Group.m
+//  Letstream
+//
+//  Created by Hans Andersson on 11/06/14.
+//  Copyright (c) 2011 Ultramentem & Vigorware. All rights reserved.
+//
+
+#import "Group.h"
+#import "Search.h"
+
+@implementation Group
+@dynamic identifier;
+@dynamic searches;
+
+@end
